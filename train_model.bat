159ad34aa24bd1252319c458899f5c1028ee8d50
jumproper <dylanmplummer@gmail.com>
2018-08-16 21:13:29 -0400

    Add camera randomization

--- train_model.bat
+++ train_model.bat
@@ -1,2 +1,2 @@
-opencv_traincascade -data data -vec positives.vec -numPos 2000 -numNeg 1000 -bg bg.txt -numStages 20
+opencv_traincascade -data data -vec positives.vec -numPos 1800 -numNeg 900 -bg bg.txt -numStages 6 -w 30 -h 30
 pause
